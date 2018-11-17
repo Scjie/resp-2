e77058676945f1a676fb210f72a5f2a30582e1d3
scjie <351414591@qq.com>
2018-11-17 21:15:04 +0800

    comit

---
+++ ModuleProject/eureka-server/src/test/java/com/cullinan/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.cullinan.eurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@EnableEurekaServer
+public class EurekaServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
